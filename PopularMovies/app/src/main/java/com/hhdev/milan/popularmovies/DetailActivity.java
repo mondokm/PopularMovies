db913a99841f8087147cce3f992e08904f050c54
mondokm <milanmondok1998@gmail.com>
2017-03-09 23:10:52 +0100

    Reviews query added

--- PopularMovies/app/src/main/java/com/hhdev/milan/popularmovies/DetailActivity.java
+++ PopularMovies/app/src/main/java/com/hhdev/milan/popularmovies/DetailActivity.java
@@ -1,18 +1,24 @@
 package com.hhdev.milan.popularmovies;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DetailActivity extends AppCompatActivity{
+
+    JSONObject data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,7 @@ public class DetailActivity extends AppCompatActivity{
 
         Intent intent = getIntent();
         try {
-            JSONObject data = new JSONObject(intent.getExtras().getString(Intent.EXTRA_TEXT));
+            data = new JSONObject(intent.getExtras().getString(Intent.EXTRA_TEXT));
             setTitle(data.getString(NetworkTools.TITLE));
             ((TextView) findViewById(R.id.overview)).setText(data.getString(NetworkTools.OVERVIEW));
             ((TextView) findViewById(R.id.rating)).setText(data.getString(NetworkTools.VOTE_AVERAGE)+getString(R.string.vote_average));
@@ -40,6 +46,52 @@ public class DetailActivity extends AppCompatActivity{
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onReviewButtonClick(View v){
+        try {
+            new FetchReviewsTask().execute(NetworkTools.BASE_URL+"/"+data.getString(NetworkTools.ID)+"/"+NetworkTools.REVIEWS,1+"");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class FetchReviewsTask extends AsyncTask<String,Void,JSONObject[]> {
+
+        protected void onPreExecute(){
+            super.onPreExecute();
+
+        }
+
+        protected void onPostExecute(JSONObject[] data){
+                seeReviews(data);
+        }
+
+        protected JSONObject[] doInBackground(String[] params){
+            if(params.length == 0){
+                return null;
+            }
+            try{
+                String jsonResponse = NetworkTools.getResponseFromHTTP(NetworkTools.buildUrl(params[0], getString(R.string.themoviedb_key_v3),params[1])); //insert your own key here
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                JSONArray results = jsonObject.getJSONArray(NetworkTools.RESULTS);
+                JSONObject[] reviews = new JSONObject[results.length()];
+                for(int i = 0;i<results.length();i++){
+                    reviews[i] = results.getJSONObject(i);
+                }
+                return reviews;
+
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
+    }
+
+    public void seeReviews(JSONObject[] data){
+        
     }
 
 }
--- PopularMovies/app/src/main/java/com/hhdev/milan/popularmovies/NetworkTools.java
+++ PopularMovies/app/src/main/java/com/hhdev/milan/popularmovies/NetworkTools.java
@@ -10,17 +10,20 @@ import java.util.Scanner;
 
 class NetworkTools {
 
+    public static final String BASE_URL = "https://api.themoviedb.org/3/movie";
     public static final String POPULAR_URL = "https://api.themoviedb.org/3/movie/popular";
     public static final String TOP_RATED_URL = "https://api.themoviedb.org/3/movie/top_rated";
     public static final String THE_MOVIE_DB_IMG_URL = "http://image.tmdb.org/t/p/w342/";
     public static final String RESULTS = "results";
     public static final String OVERVIEW = "overview";
     public static final String TITLE = "title";
+    public static final String REVIEWS = "reviews";
     public static final String VOTE_AVERAGE = "vote_average";
     public static final String RELEASE_DATE = "release_date";
     public static final String POSTER_PATH = "poster_path";
     public static final String API_KEY = "api_key";
     public static final String PAGE = "page";
+    public static final String ID = "id";
 
 
 
